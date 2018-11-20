55ff005ffe4bc1a0d69154af9f37ab5fc965cf26
jmrf <jose.marcos.rf@gmail.com>
2018-11-20 21:55:36 +0000

    update python voice action server

--- voice_assistant/voice_action_server.py
+++ voice_assistant/voice_action_server.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import argparse
 import datetime
 import logging
 import pprint
@@ -26,7 +27,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(MAIN_LIGHTS_PIN, GPIO.OUT)	# map to physical ports
 
 
-logging.basicConfig()
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
@@ -38,9 +39,9 @@ def time_now():
 mqtt_client = mqtt.Client()
 
 # define the name intents
-TURN_LIGHTS_ON = 'hermes/intent/user_ryqs-JIGb__TornOnLights'
-TURN_LIGHTS_OFF = 'hermes/intent/user_ryqs-JIGb__TurnOffLights'
-DIM_LIGHTS = 'hermes/intent/user_ryqs-JIGb__DimLights'
+TURN_LIGHTS_ON = 'hermes/intent/jmrf:TurnOnLights'
+TURN_LIGHTS_OFF = 'hermes/intent/jmrf:TurnOffLights'
+DIM_LIGHTS = 'hermes/intent/jmrf:DimLights'
 ASSISTANT_MSG = 'hermes/tts/say'
 
 
@@ -52,34 +53,51 @@ def on_connect(client, userdata, flags, rc):
     mqtt_client.subscribe(ASSISTANT_MSG)
 
 
-def get_prob(msg):
-        return json.loads(msg.payload).get('intent').get('probability')
+def get_prob(payload):
+        return payload.get('intent').get('probability')
 
 
 # Process a message as it arrives
 def on_message(client, userdata, msg):
+    try:
+        # payload contains all the captured information from the ASR
+        payload = json.loads(msg.payload.decode("utf-8"))
+        logger.debug("Message payload: {}".format(pprint.pformat(payload)))
 
-    payload = json.loads(msg.payload)
-    logger.debug(pprint.pformat(payload))
-    light_type = [s['rawValue'] for s in payload.get('slots', [])
-                  if s['slotName'] == 'LightType'] or '???'
+        # message topic corresponds to the MQTT topic, which contains the intent-name
+        topic = msg.topic
+        logger.info("Message topic: {}".format(topic.split("/")[-1]))
 
-    if msg.topic == TURN_LIGHTS_ON:
-        logger.info(("[{}] - Understood should turn on the {} lights."
-                     " (prob: {:.3f})").format(time_now(), light_type, get_prob(msg)))
-        GPIO.output(MAIN_LIGHTS_PIN, GPIO.HIGH)
-    elif msg.topic == TURN_LIGHTS_OFF:
-        logger.info(("[{}] - Understood should turn off the {} lights."
-                     " (prob: {:.3f})").format(time_now(), light_type, get_prob(msg)))
-        GPIO.output(MAIN_LIGHTS_PIN, GPIO.LOW)
-    elif msg.topic == DIM_LIGHTS:
-        logger.info(("[{}] - Understood should dim the {} lights."
-                     " (prob: {:.3f})").format(time_now(), light_type, get_prob(msg)))
-    elif msg.topic == ASSISTANT_MSG:
-        logger.info("[{}] - {}".format(time_now(), json.loads(msg.payload).get('text')))
+        light_type = [s['rawValue'] for s in payload.get('slots', [])
+                      if s['slotName'] == 'LightType'] or '???'
+
+        if msg.topic == TURN_LIGHTS_ON:
+            logger.info(("Understood should turn on the {} lights."
+                         " (prob: {:.3f})").format(light_type, get_prob(payload)))
+            GPIO.output(MAIN_LIGHTS_PIN, GPIO.HIGH)
+        elif msg.topic == TURN_LIGHTS_OFF:
+            logger.info(("Understood should turn off the {} lights."
+                         " (prob: {:.3f})").format(light_type, get_prob(payload)))
+            GPIO.output(MAIN_LIGHTS_PIN, GPIO.LOW)
+        elif msg.topic == DIM_LIGHTS:
+            logger.info(("Understood should dim the {} lights."
+                         " (prob: {:.3f})").format(light_type, get_prob(payload)))
+        elif msg.topic == ASSISTANT_MSG:
+            logger.info("Assistant says: '{}'".format(payload.get('text')))
+    except Exception as e:
+        logger.error("Error @ 'on_message': {}".format(e))
+        logger.exception(e)
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mqtt_server", default="localhost", help="MQTT server address")
+    parser.add_argument("--mqtt_port", default=1883, help="MQTT server port")
+    return parser.parse_args()
 
 
-mqtt_client.on_connect = on_connect
-mqtt_client.on_message = on_message
-mqtt_client.connect('localhost', 9898)
-mqtt_client.loop_forever()
+if __name__ == "__main__":
+    args = get_args()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    mqtt_client.connect(args.mqtt_server, args.mqtt_port)
+    mqtt_client.loop_forever()
