d712c2ae75fd863e767b54820ea563385c590d56
Jose Marcos Rodríguez Fernández <jose.marcos.rf@gmail.com>
2021-11-12 00:22:35 +0100

    formatting 🤓

--- hotword/run.py
+++ hotword/run.py
@@ -1,9 +1,9 @@
 from precise_runner import PreciseEngine, PreciseRunner
 
 model_pb = "models/phiona/phiona.pb"
-engine_exec = "/home/jose/code/Personal/RaspVan/hotword/precise/precise-engine"
+engine_binary = "/home/jose/code/Personal/RaspVan/hotword/precise/precise-engine"
 
-engine = PreciseEngine(engine_exec, model_pb)
+engine = PreciseEngine(engine_binary, model_pb)
 runner = PreciseRunner(engine, on_activation=lambda: print("hello"))
 runner.start()
 
