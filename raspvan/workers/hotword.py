24cc2765fc9832fa02291c0f77b23216c5eda66f
jmrf <jose.marcos.rf@gmail.com>
2021-11-16 01:01:43 +0100

    rpi hotword run WiP

--- raspvan/workers/hotword.py
+++ raspvan/workers/hotword.py
@@ -4,8 +4,10 @@ import logging
 import threading
 
 from playsound import playsound
+from pyaudio import PyAudio, paInt16
 from time import sleep
 
+from raspvan.constants import AUDIO_DEVICE_ID_ENV_VAR
 from raspvan.constants import Q_EXCHANGE_ENV_VAR
 from common.utils.io import init_logger
 from common.utils.rabbit import BlockingQueuePublisher
@@ -16,9 +18,10 @@ from precise_runner import PreciseRunner
 
 
 logger = logging.getLogger(__name__)
-init_logger(level=logging.INFO, logger=logger)
+init_logger(level=logging.DEBUG, logger=logger)
 
 Q_TOPIC = "hotword"
+CHUNK_SIZE = 2048
 COUNT = 0
 
 
@@ -29,9 +32,7 @@ class SoundThread(threading.Thread):
 
     def run(self):
         try:
-            sound_path = (
-                "/home/jose/code/Personal/RaspVan/assets/diagrams/hotword-ding-2.mp3"
-            )
+            sound_path = os.path.join(os.getcwd(), "assets/hotword-ding-2.mp3")
             playsound(sound_path)
         except Exception as e:
             logger.error(f"Error playing sound: {e}")
@@ -70,24 +71,42 @@ def run():
     # Init the rabbit MQ sender
     logger.info("🐇 Initializing publisher")
     publisher = BlockingQueuePublisher(
-        amqp_uri="localhost",
+        amqp_uri="localhost",  # TODO: Make this configurable!
         exchange_name=xchange,
         exchange_type="topic",
     )
 
-    trigger = Trigger(publisher)
-
-    # Init the Precise Engine & Runner
+    # Init the Precise Engine
     logger.info("📢 Initializing hotword engine")
-    engine = PreciseEngine(engine_binary, model_pb)
-    runner = PreciseRunner(engine, on_activation=trigger.on_activation)
+    engine = PreciseEngine(engine_binary, model_pb, chunk_size=CHUNK_SIZE)
+
+    # Init the runner with a custom stream
+    # so we can select the device
+    device_id = os.getenv(AUDIO_DEVICE_ID_ENV_VAR, 0)
+    logger.debug(f"Using device ID: {device_id}")
+    pa = PyAudio()
+    stream = pa.open(
+        rate=16000,
+        channels=4,
+        format=paInt16,
+        input=True,
+        frames_per_buffer=CHUNK_SIZE,
+        input_device_index=int(device_id),
+    )
+    trigger = Trigger(publisher)
+    runner = PreciseRunner(engine, on_activation=trigger.on_activation, stream=stream)
 
     logger.info("🚀 Ignition")
     runner.start()
 
+    # trigger.on_activation()
+
     # Sleep forever
     while True:
         sleep(10)
+
+    pa.terminate()
+    stream.stop_stream()
 
 
 if __name__ == "__main__":
