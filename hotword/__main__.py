ce15ac5fd95f2751aa68b0428fbab106198947f6
jmrf <jose.marcos.rf@gmail.com>
2024-12-25 19:35:01 +0100

    add requirements to hotword

--- hotword/__main__.py
+++ hotword/__main__.py
@@ -15,44 +15,35 @@ from raspvan.constants import (
 )
 from respeaker.pixels import Pixels
 
-print(f"Precise Runner version: {precise_runner.__version__}")
-
-model_pb = os.getenv("HOTWORD_MODEL")
-if model_pb is None:
-    raise ValueError("'HOTWORD_MODEL' env. var not set.")
-
-engine_binary = os.getenv("PRECISE_ENGINE")
-if engine_binary is None:
-    raise ValueError("'PRECISE_ENGINE' env. var not set.")
-
-
 
 @click.command()
 @click.option(
     "engine-binary",
     type=click.Path(dir_okay=False),
-    default=os.getenv(PRECISE_ENGINE_ENV_VAR, PRECISE_ENGINE_DEFAULT_BIN_PATH)
+    default=os.getenv(PRECISE_ENGINE_ENV_VAR, PRECISE_ENGINE_DEFAULT_BIN_PATH),
 )
 @click.option(
     "model-pb",
     type=click.Path(dir_okay=False),
-    default=os.getenv(HOTWORD_MODEL_ENV_VAR, HOTWORD_MODEL_DEFAULT_PATH)
+    default=os.getenv(HOTWORD_MODEL_ENV_VAR, HOTWORD_MODEL_DEFAULT_PATH),
 )
 def main(engine_binary, model_pb):
+    print(f"ℹ️ Precise Runner version: {precise_runner.__version__}")
+
     pixels = Pixels(pattern_name="google")
     engine = PreciseEngine(engine_binary, model_pb)
     runner = PreciseRunner(
-        engine,
-        on_activation=lambda: pixels.wakeup() and print("👋🏼 Hello")
+        engine, on_activation=lambda: pixels.wakeup() and print("👋🏼 Hello")
     )
     runner.start()
 
     with Halo(text="👂🏼 Listening...") as spinner:
         while True:
-            print(random.choice(["😪", "😴", "💤"])
+            print(random.choice(["😪", "😴", "💤"]))
             time.sleep(10)
 
+        spinner.success("✅ Done!")
 
-if __name__ == "_main__":
+
+if __name__ == "__main__":
     main()
-
